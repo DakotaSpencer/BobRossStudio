72c6152878cdb5fff9cdbe5a916406379f133df2
fireball5559 <ohernadezpalacios@student.neumont.edu>
2022-05-16 15:00:48 -0600

    Grabbing and freezing

---
+++ Assets/Painting/Scripts/MoveAndFreeze.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class MoveAndFreeze : MonoBehaviour
+{
+
+    private Rigidbody m_body;
+    bool keypressCheckF;
+    bool keypressCheckC;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        m_body = GetComponent<Rigidbody>();
+        m_body.constraints = RigidbodyConstraints.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        keypressCheckF = Input.GetKeyDown(KeyCode.F);
+        keypressCheckC = Input.GetKeyDown(KeyCode.C);
+        if (keypressCheckF) 
+        {
+
+            m_body.constraints = RigidbodyConstraints.FreezeAll;
+            return;
+        }
+    }
+    
+}
