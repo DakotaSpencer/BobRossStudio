67b7bb2cd6bad374c4ddbb1f27906421da8bb504
ethcala <81577564+ethcala@users.noreply.github.com>
2022-05-16 21:03:11 -0600

    linking menu buttons to other menus

--- Assets/Menus/MainMenu.cs
+++ Assets/Menus/MainMenu.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void QuitGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Quitting game.");
+        Application.Quit();
     }
 }
